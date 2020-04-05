ef2eafa12a404e5fdc67e1ef21269ba99068b036
Automage <pranav92000@gmail.com>
2020-04-05 04:39:59 -0500

    initial implementation of script interface

--- script.cc
+++ script.cc
@@ -1,11 +1,89 @@
+#include <stdlib.h>
+#include <string.h>
+
 #include "script.h"
 
 #define MAIN_BUF_SIZE 1000
+#define DEFAULT_BUF_SIZE 5
 
-void layout_init(struct script_layout *layout) {
-  if (layout == NULL) {
+void layout_init(struct script_layout *script) {
+  if (script == NULL) {
     return;
   }
 
-  layout->main_body = malloc(sizeof(char) * FUNCTION_BUF_SIZE);
+  script->main_body = malloc(sizeof(char) * MAIN_BUF_SIZE);
+  script->defines = malloc(sizeof(char *) * DEFAULT_BUF_SIZE);
+  script->includes = malloc(sizeof(char *) * DEFAULT_BUF_SIZE);
+  script->functions = malloc(sizeof(char *) * DEFAULT_BUF_SIZE);
+
+  script->main_size = 0;
+  script->n_defines = 0;
+  script->n_includes = 0;
+  script->n_functions = 0;
+
+  script->max_main_buf = MAIN_BUF_SIZE;
+  script->max_defines = DEFAULT_BUF_SIZE;
+  script->max_includes = DEFAULT_BUF_SIZE;
+  script->max_functions = DEFAULT_BUF_SIZE;
+}
+
+void append_main(struct script_layout *script, char *line) {
+  if (line == NULL) {
+    return;
+  }
+
+  if (script->main_size + strlen(line) >= script->max_main_buf) {
+    // Reallocation
+  }
+
+  strcpy(script->main_body, line);
+  script->main_size += strlen(line);
+}
+
+int add_define(struct script_layout *script, char *content) {
+  if (content == NULL) {
+    return -1;
+  }
+
+  if (script->n_defines + 1 >= script->max_defines) {
+    // Reallocation
+  }
+
+  script->defines[script->n_defines] = content;
+  script->n_defines++;
+}
+
+int add_include(struct script_layout *script, char *content) {
+  if (content == NULL) {
+    return -1;
+  }
+  if (script->n_includes + 1 >= script->max_includes) {
+    // Reallocation
+  }
+
+  script->includes[script->n_includes] = content;
+  script->n_includes++;
+}
+
+int add_function(struct script_layout *script, char *content) {
+  if (content == NULL) {
+    return -1;
+  }
+
+  if (script->n_functions + 1 >= script->max_functions) {
+    // Reallocation
+  }
+
+  script->functions[script->n_functions] = content;
+  script->n_functions++;
+}
+
+/*
+ * Generate Source
+ *
+ * Write to the file specified by argument the final 
+ * C code to be compiled, as infered from the cscript.
+ */
+int generate_source(FILE *out) {
+  return;
 }
--- script.hh
+++ script.hh
@@ -1,6 +1,8 @@
 #ifndef SCRIPT_H
 #define SCRIPT_H
 
+#include <stdio.h>
+
 struct script_layout {
   char *main_body;
 
@@ -8,11 +10,22 @@ struct script_layout {
   char **includes;
   char **functions;
 
+  int main_size;
   int n_defines;
   int n_includes;
   int n_functions;
+
+  int max_main_buf;
+  int max_defines;
+  int max_includes;
+  int max_functions;
 };
 
 void layout_init(struct script_layout *);
+void append_main(struct script_layout, char *);
+int add_define(struct script_layout, char *);
+int add_include(struct script_layout, char *);
+int add_function(struct script_layout, char *);
+int generate_source(FILE *);
 
 #endif
