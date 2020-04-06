3beea3424b978bf12a56074852a0931f5cd3c935
Automage <pranav92000@gmail.com>
2020-04-06 04:49:53 -0500

    regex implementation

--- parser.c
+++ parser.c
@@ -20,25 +20,28 @@ int parse_script(struct script_layout *script, FILE *in_file) {
 
   const char DEFINE_REGEX[] = "^#define .*$";
   const char INCLUDE_REGEX[] = "^#include .*$";
-  const char FUNCTION_REGEX[] = "^\\w+ \\w+\\((( *\\w+ \\w+)?(, *\\w+ \\w+)*)?\\)$";
+  const char FUNCTION_REGEX[] = "^\\w*$";
+  //const char FUNCTION_REGEX[] = "^\\w+ \\w+\\((( *\\w+ \\w+)?(, *\\w+ (\\w|\\[|\\])+)*)?\\) *\\{?$";
+  //const char FUNCTION_REGEX[] = "^(\\w|\\[|\\]|\\*)+ (\\w|\\*)+\\((( *(\\w|\\[|\\]|\\*)+ (\\w|\\[|\\]|\\*)+)?(, *(\\w|\\[|\\]|\\*)+ (\\w|\\[|\\]|\\*)+)*)?\\) *\\{?$";
+  printf("reg: %s\n", FUNCTION_REGEX);
 
   // Regex intialization
 
   // Define regex
   regex_t define_re;
-  if (regcomp(&define_re, DEFINE_REGEX, REG_EXTENDED) == -1) {
+  if (regcomp(&define_re, DEFINE_REGEX, REG_EXTENDED | REG_NOSUB) == -1) {
     return -1;
   }
 
   // Include regex
   regex_t include_re;
-  if (regcomp(&include_re, INCLUDE_REGEX, REG_EXTENDED) == -1) {
+  if (regcomp(&include_re, INCLUDE_REGEX, REG_EXTENDED | REG_NOSUB) == -1) {
     return -1;
   }
 
   // Function regex
   regex_t function_re;
-  if (regcomp(&function_re, FUNCTION_REGEX, REG_EXTENDED) == -1) {
+  if (regcomp(&function_re, FUNCTION_REGEX, REG_ENHANCED | REG_NOSUB) == -1) {
     return -1;
   }
 
@@ -47,6 +50,21 @@ int parse_script(struct script_layout *script, FILE *in_file) {
   char line[MAX_LINE_SIZE];
   while (fscanf(in_file, "%[^\n]\n", line) == 1) {
     printf("%s\n", line);
+
+    // Match defines
+    if (regexec(&define_re, line, 0, NULL, 0) == 0) {
+      printf("-----DEF detected\n");
+    }
+
+    // Match includes
+    if (regexec(&include_re, line, 0, NULL, 0) == 0) {
+      printf("-----INCLD detected\n");
+    }
+
+    // Match functions
+    if (regexec(&function_re, line, 0, NULL, 0) == 0) {
+      printf("-----FUNC detected\n");
+    }
   }
 
   return 1;
